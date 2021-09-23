aed7fed42fa37a26224eddaea17503f50349ec75
GreenScreen410 <pauljjang410@gmail.com>
2021-09-24 00:47:46 +0900

    Edited ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const client = require("../index");
 
 client.on("ready", () =>
+    console.log(`${client.user.tag} is up and ready to go!`),
     client.user.setActivity("24시간 명령어", { type: 'LISTENING' }),
-    console.log(`${client.user.tag} is up and ready to go!`)
 );

c3e2363e5e1c00d75dd6a9ab89ee5557a321ddc9
GreenScreen410 <pauljjang410@gmail.com>
2021-09-24 00:40:17 +0900

    Edited ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 const client = require("../index");
 
 client.on("ready", () =>
+    client.user.setActivity('24시간 명령어', { type: 'LISTENING' }),
     console.log(`${client.user.tag} is up and ready to go!`)
 );

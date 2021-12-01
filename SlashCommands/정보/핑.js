06a5b06af170d920e5543b65aab2bc86f198ec86
GreenScreen410 <pauljjang410@gmail.com>
2021-12-01 15:33:14 +0900

    Edited 핑.js

--- SlashCommands/정보/핑.js
+++ SlashCommands/정보/핑.js
@@ -5,13 +5,13 @@ module.exports = {
     description: "반응 속도를 반환합니다.",
 
     run: async (client, interaction) => {
-
         const embed = new MessageEmbed()
             .setColor("#FF0000")
             .setTitle("🏓 퐁!")
             .setDescription(`반응 속도 : ${client.ws.ping}ms`)
             .setTimestamp()
             .setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL())
+            
         interaction.followUp({ embeds: [embed] });
     },
 };

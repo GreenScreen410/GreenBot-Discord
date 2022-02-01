6eabdcf8b6d9ece6d9aa276b63941f81938b6e90
GreenScreen410 <pauljjang410@gmail.com>
2022-02-01 19:49:20 +0900

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -9,7 +9,7 @@ client.on("messageCreate", async (message) => {
         return;
 
     const [cmd, ...args] = message.content
-        .slice(client.config.prefix.length)
+        .slice(process.env.token.length)
         .trim()
         .split(/ +/g);
 

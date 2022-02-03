adf5b7317811f79c271b96475b23545e1577ee56
GreenScreen410 <pauljjang410@gmail.com>
2022-02-03 15:15:18 +0900

    Update 재생.js

--- SlashCommands/음악/재생.js
+++ SlashCommands/음악/재생.js
@@ -11,7 +11,7 @@ module.exports = {
 
     run: async (client, interaction) => {
 
-        // 노래 제목을 songTitle이라는 변수에 저장
+        // 인자(Argument)로 받아온 제목을 songTitle이라는 변수에 저장
         const songTitle = interaction.options.getString("노래");
 
         // 음성 채널 접속 여부 확인
@@ -20,15 +20,20 @@ module.exports = {
             return;
         }
 
+
+        // songTitle 변수를 이용해 searchResult라는 검색 결과 변수 생성
         const searchResult = await player.search(songTitle, {
             requestedBy: interaction.user,
             searchEngine: QueryType.AUTO
         });
 
+        // queue라는 이름의 재생목록 변수 생성
         const queue = await player.createQueue(interaction.guild, {
             metadata: interaction.channel
         });
 
+
+        // 만약 음성 채널에 접속해있지 않는 상황이라면, 음성 채널에 접속
         if(!queue.connection) {
             await queue.connect(interaction.member.voice.channel);
         }
@@ -39,6 +44,8 @@ module.exports = {
             ? queue.addTrack(searchResult.tracks)
             : queue.addTrack(searchResult.tracks[0]);
 
+
+        // 음악을 재생하고 있지 않는 상황이라면, 음악 재생
         if(!queue.playing) {
             await queue.play();
         }

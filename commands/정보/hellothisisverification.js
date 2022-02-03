0efbb11adb9fcdec98fde92c0073abde85f9e4ce
GreenScreen410 <pauljjang410@gmail.com>
2022-02-03 20:36:25 +0900

    Create hellothisisverification.js

---
+++ commands/정보/hellothisisverification.js
@@ -0,0 +1,9 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "hellothisisverification",
+
+    run: async (client, message, args) => {
+        message.channel.send("그린스크린#2647");
+    },
+};

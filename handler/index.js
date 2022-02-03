ab791ded99330547942bcb34bf983f0b614a6d7f
GreenScreen410 <pauljjang410@gmail.com>
2022-02-03 15:33:47 +0900

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -54,9 +54,7 @@ module.exports = async (client) => {
 
     client.on("ready", async () => {
         // Register for a single guild
-        await client.guilds.cache
-            .get("825741743235268639")
-            .commands.set(arrayOfSlashCommands);
+        await client.guilds.cache.get("825741743235268639").commands.set(arrayOfSlashCommands);
 
         // Register for all the guilds the bot is in
         // await client.application.commands.set(arrayOfSlashCommands);

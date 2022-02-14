5f578a032087eed8dbdd42a18b65d4617293989c
GreenScreen410 <pauljjang410@gmail.com>
2022-02-15 01:12:56 +0900

    v1.0.6

--- SlashCommands/음악/재생.js
+++ SlashCommands/음악/재생.js
@@ -16,11 +16,15 @@ module.exports = {
   ],
 
   run: async (client, interaction) => {
+    // songTitle이라는 변수에 입력한 노래 제목을 저장
     const songTitle = interaction.options.getString("노래");
 
     // 음성 채널에 접속해 있지 않을 때
     if (!interaction.member.voice.channel) {
+      // ERROR 파일의 PLEASE_JOIN_VOICE_CHANNEL 함수 실행
       ERROR.PLEASE_JOIN_VOICE_CHANNEL(client, interaction);
+
+      // 코드가 더 이상 실행되지 않도록 방지
       return;
     }
 
@@ -28,23 +32,37 @@ module.exports = {
     if (
       interaction.guild.me.voice.channelId &&
       interaction.member.voice.channelId !==
-      interaction.guild.me.voice.channelId
+        interaction.guild.me.voice.channelId
     ) {
+      // ERROR 파일의 PLEASE_JOIN_SAME_VOICE_CHANNEL 함수 실행
       ERROR.PLEASE_JOIN_SAME_VOICE_CHANNEL(client, interaction);
+
+      // 코드가 더 이상 실행되지 않도록 방지
       return;
     }
 
+    // queue라는 이름의 재생목록 변수 생성
     const queue = await player.createQueue(interaction.guild, {
       metadata: interaction.channel,
     });
 
+    // 음성 채널 연결 시도
     try {
+      // 음성 채널에 연결되어 있지 않는다면
       if (!queue.connection) {
+        // 재생목록을 현재 위치한 음성 채널과 연결
         await queue.connect(interaction.member.voice.channel);
       }
+
+      // 음성 채널 연결 실패 시
     } catch (error) {
+      // 재생목록 삭제
       queue.destroy();
+
+      // ERROR 파일의 CAN_NOT_JOIN_VOICE_CHANNEL 함수 실행
       ERROR.CAN_NOT_JOIN_VOICE_CHANNEL(client, interaction);
+
+      // 코드가 더 이상 실행되지 않도록 방지
       return;
     }
 
@@ -53,20 +71,26 @@ module.exports = {
       searchEngine: QueryType.AUTO,
     });
 
+    // 노래를 찾지 못하였을 때
     if (!track || !track.tracks.length) {
+
+      // ERROR 파일의 CAN_NOT_FIND_MUSIC 함수 실행
       ERROR.CAN_NOT_FIND_MUSIC(client, interaction);
+
+      // 코드가 더 이상 실행되지 않도록 방지
       return;
     }
 
     const embed = new MessageEmbed()
       .setColor("RANDOM")
       .setTitle(
-        `🎶 ${track.playlist ? "playlist" : "재생목록에 추가되었습니다."}`)
+        `🎶 ${track.playlist ? "playlist" : "재생목록에 추가되었습니다."}`
+      )
       .setTimestamp()
       .setFooter({
         text: `Requested by ${interaction.user.tag}`,
         iconURL: `${interaction.user.displayAvatarURL()}`,
-      })
+      });
 
     if (!track.playlist) {
       const tr = track.tracks[0];
--- SlashCommands/음악/재생목록.js
+++ SlashCommands/음악/재생목록.js
@@ -6,9 +6,12 @@ module.exports = {
   musicQueue: function (client, interaction) {
     const queue = player.getQueue(interaction.guildId);
 
-    // 재생중인 노래가 없을 때
+    // 재생목록이 없을 때 또는(||) 재생중인 노래가 없을 때
     if (!queue || !queue.playing) {
+      // ERROR 파일의 MUSIC_QUEUE_IS_EMPTY 함수 실행
       ERROR.MUSIC_QUEUE_IS_EMPTY(client, interaction);
+
+      // 코드가 더 이상 실행되지 않도록 방지
       return;
     }
 

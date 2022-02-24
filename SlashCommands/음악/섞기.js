06b98006cdd61fc83a8a80e722c5ae1de96d0e9e
GreenScreen410 <pauljjang410@gmail.com>
2022-02-25 01:06:19 +0900

    v1.0.13

--- SlashCommands/음악/섞기.js
+++ SlashCommands/음악/섞기.js
@@ -1,12 +1,12 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const player = require("../../events/player");
 const ERROR = require("../ERROR");
 
 module.exports = {
   ...new SlashCommandBuilder()
-  .setName("재생목록초기화")
-  .setDescription("노래 재생목록을 초기화합니다."),
+    .setName("섞기")
+    .setDescription("노래 재생목록을 랜덤하게 섞습니다."),
 
   run: function (client, interaction) {
     const queue = player.getQueue(interaction.guildId);
@@ -14,14 +14,18 @@ module.exports = {
       return ERROR.MUSIC_QUEUE_IS_EMPTY(client, interaction);
     }
 
-    queue.clear();
+    queue.shuffle();
 
     const embed = new MessageEmbed()
       .setColor("RANDOM")
-      .setTitle("💥 펑!")
-      .setDescription("재생목록이 초기화되었습니다!")
+      .setTitle("🔀 셔플 완료!")
+      .setDescription("재생목록이 랜덤하게 섞였습니다. 한번 확인해 보세요!")
       .setTimestamp()
       .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });
-    interaction.followUp({ embeds: [embed] });
+
+    const button = new MessageActionRow().addComponents(
+      new MessageButton().setCustomId("musicQueue").setEmoji("📄").setLabel("재생목록").setStyle("PRIMARY"),
+    );
+    interaction.followUp({ embeds: [embed], components: [button] });
   },
 };

ee88a78f35390d708e5ebeb05a663b89794d8555
GreenScreen410 <pauljjang410@gmail.com>
2022-03-01 16:01:22 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const client = require("../index");
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is up and ready to go!`);
-  client.user.setActivity({ name: "24시간 명령어", type: "LISTENING" });
+  client.user.setActivity({ name: "24/7 명령어", type: "LISTENING" });
 });

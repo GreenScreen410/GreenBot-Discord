9907ce14e51950cad15810c8dcdb81e7c9072bcc
GreenScreen410 <pauljjang410@gmail.com>
2022-03-05 13:52:02 +0900

    :hammer: 봇 상태 수정

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const client = require("../index");
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is up and ready to go!`);
-  client.user.setActivity({ name: "24/7 명령어", type: "LISTENING" });
+  client.user.setActivity(`${({ name: `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}명의 유저`, type: "WATCHING" })}`);
 });

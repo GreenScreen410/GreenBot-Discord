0d09a6b5de1736423992e665d4e08646806080a0
GreenScreen410 <pauljjang410@gmail.com>
2022-03-19 11:31:31 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const client = require("../index");
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is up and ready to go!`);
-  client.user.setActivity(`${({ name: `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}명의 유저`, type: "WATCHING" })}`);
+  client.user.setActivity(`${({ name: `/명령어`, type: "WATCHING" })}`);
 });

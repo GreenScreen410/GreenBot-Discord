7b6094f92d101ecdce4f7261993cf78550b1618a
GreenScreen410 <pauljjang410@gmail.com>
2022-03-22 21:36:33 +0900

    :heavy_plus_sign: koreanbots 모듈 추가

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const { Client, Collection } = require("discord.js");
+const { KoreanbotsClient } = require("koreanbots");
 
-const client = new Client({ intents: 32767 });
+const client = new KoreanbotsClient({
+  intents: 32767,
+  koreanbots: {
+    api: {
+      token: process.env.KOREANBOTS_TOKEN
+    }
+  }, koreanbotsClient: {
+    updateInterval: 600000
+  }
+});
 module.exports = client;
 
 // Global Variables
-client.commands = new Collection();
+// client.commands = new Collection();
 client.slashCommands = new Collection();
 
 // Initializing the project

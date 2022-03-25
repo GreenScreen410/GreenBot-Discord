fdbec02de6230507632039b32cd3fc5cc61166e6
GreenScreen410 <pauljjang410@gmail.com>
2022-03-25 22:06:45 +0900

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -5,6 +5,19 @@ const mongoose = require("mongoose");
 const globPromise = promisify(glob);
 
 module.exports = async (client) => {
+  // Commands
+  // const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
+  /* commandFiles.map((value) => {
+      const file = require(value);
+      const splitted = value.split("/");
+      const directory = splitted[splitted.length - 2];
+
+      if (file.name) {
+          const properties = { directory, ...file };
+          client.commands.set(file.name, properties);
+      }
+  }); */
+
   // Slash Commands
   const slashCommands = await globPromise(`${process.cwd()}/commands/*/*.js`);
   const arrayOfSlashCommands = [];

0ed1facb8788657349b3b09be614cafba5bd85af
GreenScreen410 <pauljjang410@gmail.com>
2022-05-14 11:58:54 +0900

    Create translate.js

---
+++ handler/translate.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+module.exports = {
+  papago: async function(source, target, text) {
+    let translatedText = await axios({
+      method: "POST",
+      url: "https://openapi.naver.com/v1/papago/n2mt",
+      headers: {
+        "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
+        "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET
+      },
+
+      data: {
+        source: `${source}`,
+        target: `${target}`,
+        text: `${text}`
+      },
+    });
+
+    translatedText = JSON.parse(JSON.stringify(translatedText.data));
+    translatedText = translatedText.message.result.translatedText
+
+    return translatedText;
+  }
+};

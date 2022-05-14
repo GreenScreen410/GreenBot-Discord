bcd092b038919ee29bd7aa1ae40275d734df64fa
GreenScreen410 <63690925+GreenScreen410@users.noreply.github.com>
2022-05-14 14:45:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,8 @@ const client = new KoreanbotsClient({
 });
 module.exports = client;
 
-// Global Variables
-// client.commands = new Collection();
 client.slashCommands = new Collection();
 
-// Initializing the project
 require("./handler")(client);
 
 client.login(process.env.TOKEN);

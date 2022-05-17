623eac503bbc24307545ab51809e9a94901b5cd4
GreenScreen410 <pauljjang410@gmail.com>
2022-05-18 00:50:44 +0900

    v13.7 Testing

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,14 +2,15 @@ const client = require("../index");
 const ERROR = require("../commands/ERROR");
 
 client.on("interactionCreate", async (interaction) => {
-  // Slash Command Handling
   if (interaction.isCommand()) {
-    await interaction.deferReply({ ephemeral: false }).catch(() => { });
-
     const cmd = client.slashCommands.get(interaction.commandName);
     if (!cmd) {
       ERROR.INVALID_INTERACTION(client, interaction);
       return;
+    }
+
+    if (!cmd.modal) {
+      await interaction.deferReply({ ephemeral: false }).catch(() => { });
     }
 
     const args = [];
@@ -27,12 +28,17 @@ client.on("interactionCreate", async (interaction) => {
     cmd.run(client, interaction, args);
   }
 
-  // Context Menu Handling
   if (interaction.isContextMenu()) {
     await interaction.deferReply({ ephemeral: false });
     const command = client.slashCommands.get(interaction.commandName);
     if (command) command.run(client, interaction);
   }
-  
+
+  if (!interaction.isModalSubmit()) return;
+  await interaction.deferReply({ ephemeral: true });
+  const paragraph = interaction.fields.getTextInputValue('suggestion');
+  interaction.editReply({ content: "소중한 의견 감사합니다! 차후 버그나 의견이 반영되면 GitHub에 크레딧이 추가됩니다." })
+  console.log({ userID: interaction.user.id, userName: interaction.user.tag, paragraph });
+
   console.log(`${interaction.commandName} was ran by ${interaction.user.tag}(${interaction.user.id})`);
 });

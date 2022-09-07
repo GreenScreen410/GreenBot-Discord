51d4857ef20c7e6dbcea91e7fd49e44055307b75
GreenScreen410 <pauljjang410@gmail.com>
2022-09-08 00:23:16 +0900

    :white_check_mark: v1.8.1

---
+++ commands/시스템/가동시간.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  ...new SlashCommandBuilder()
+    .setName("가동시간")
+    .setDescription("봇의 가동 시간을 확인합니다."),
+
+  run: async (client, interaction) => {
+    let totalSeconds = (client.uptime / 1000);
+    let days = Math.floor(totalSeconds / 86400);
+    totalSeconds %= 86400;
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds % 60);
+
+    const embed = new EmbedBuilder()
+      .setColor("Random")
+      .setTitle("🕘 가동 시간")
+      .setDescription(`${days}일, ${hours}시간, ${minutes}분 ${seconds}초`)
+      .setTimestamp()
+      .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });
+    interaction.followUp({ embeds: [embed] });
+  }
+}

6a55426bd7d8aac278213655e2600000565035a6
GreenScreen410 <pauljjang410@gmail.com>
2022-11-22 16:28:48 +0900

    :hammer: Develop Achievement Handler

--- src/commands/활동/도전과제.ts
+++ src/commands/활동/도전과제.ts
@@ -1,5 +1,6 @@
 import { Client, ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder } from "discord.js";
 import mysql from "mysql";
+import achievement from "../../achievement.json" assert { type: "json" };
 
 export default {
   data: new SlashCommandBuilder()
@@ -7,44 +8,16 @@ export default {
     .setDescription("여러 명령어들을 입력하고, 보고, 실행하며 도전과제들을 발견해보세요!"),
 
   run: async (client: Client, interaction: ChatInputCommandInteraction) => {
-    const achievements = [
-      {
-        "value": "firstStep",
-        "name": "첫걸음",
-        "description": "도전과제 메뉴에 오신것을 환영합니다!",
-        "condition": "도전과제 명령어 사용"
-      },
-
-      {
-        "value": "debugger",
-        "name": "디버거",
-        "description": "오늘도 개발자는 일합니다...",
-        "condition": "예상치 못한(Unexpected) 오류 일으키기"
-      }
-    ]
-
-    // DJ, 평범한 음악에는 만족하지 못하시는군요.
-    // 음악 필터를 10번 입력했을 경우 달성
-
-    // 첫걸음, 명령어를 처음 사용하셨군요!
-    // 아무 명령어 처음 사용
-
-    // 디버거, 오늘도 개발자는 일합니다...
-    // 그린Bot에 예상하지 못한(Unexpected) 오류 일으키기
-
-    // 학생, 내가 다니는 학교의 정보는 어떨까?
-    // 학교정보 명령어 사용
-
     const connection = mysql.createConnection({
-      host: "34.70.130.162",
+      host: `${process.env.MYSQL_HOST}`,
       user: "root",
-      password: "paul2481!",
+      password: `${process.env.MYSQL_PASSWORD}`,
       database: "greenbot-database",
     });
 
     connection.query(`SELECT * FROM achievement WHERE id=${interaction.user.id}`, function(error, result) {
-      if (result == "") { // DB에 사용자 ID가 없을 때
-        connection.query(`INSERT INTO achievement VALUES (${interaction.user.id}, 1, 0)`)
+      if (result == "") {
+        connection.query(`INSERT INTO achievement(ID, FirstStep) VALUES (${interaction.user.id}, 1)`)
         
         const embed = new EmbedBuilder()
           .setColor("Random")
@@ -52,7 +25,7 @@ export default {
           .setDescription("'첫걸음' 도전과제를 획득하셨어요!\n\n획득 조건: 도전과제 명령어 사용\n도전과제 메뉴에 오신것을 환영합니다!")
           .setTimestamp()
           .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });
-        return interaction.followUp({ embeds: [embed], ephemeral: true });
+        return interaction.followUp({ embeds: [embed] });
       }
 
       const embed = new EmbedBuilder()
@@ -60,12 +33,12 @@ export default {
         .setTitle("⭐ 획득한 도전과제")
         .setDescription(`<@${interaction.user.id}>님이 획득하신 도전과제입니다.`)
 
-      for (let i = 0; i < 2; i++) {
-        if (result[0][achievements[i].value] == 0) continue
-        embed.addFields({ name: `${achievements[i].name}`, value: `${achievements[i].condition}`, inline: true })
+      for (let i = 0; i < Object.keys(achievement).length; i++) {
+        if (result[0][achievement[i].value] == 0) continue
+        embed.addFields({ name: `${achievement[i].name}`, value: `${achievement[i].description}`, inline: true })
       }
 
-      interaction.followUp({ embeds: [embed], ephemeral: true })
+      interaction.followUp({ embeds: [embed] });
     });
   },
 };

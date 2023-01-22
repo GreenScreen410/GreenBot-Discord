d13bff8ef94e02e5e0157c1c9e41a375ce5e7b7a
GreenScreen410 <pauljjang410@gmail.com>
2023-01-22 19:05:59 +0900

    :heavy_plus_sign: feat: 차단 시스템 구축

--- src/events/interaction/interactionCreate.ts
+++ src/events/interaction/interactionCreate.ts
@@ -1,7 +1,15 @@
 import { BaseInteraction } from "discord.js";
+import mysql from "mysql";
 import chalk from "chalk";
 import client from "../../index.js"
 import ERROR from "../../handler/ERROR.js";
+
+const connection = mysql.createConnection({
+  host: `${process.env.MYSQL_HOST}`,
+  user: "root",
+  password: `${process.env.MYSQL_PASSWORD}`,
+  database: "greenbot-database",
+});
 
 client.on("interactionCreate", async (interaction: BaseInteraction) => {
   if (!interaction.isChatInputCommand() || !interaction.inCachedGuild()) return;
@@ -12,7 +20,16 @@ client.on("interactionCreate", async (interaction: BaseInteraction) => {
   if (!command) {
     return ERROR.INVALID_INTERACTION(interaction);
   } else {
-    command.run(client, interaction);
-    console.log(chalk.white(`[COMMAND] ${interaction.guild.name}(${interaction.guild.id}) - ${interaction.user.tag}(${interaction.user.id}) executed ${interaction.commandName}`))
+    connection.query(`SELECT * FROM ban WHERE ID=${interaction.user.id}`, function (error, result) {
+      try {
+        if (result[0].ban == 1) {
+          return ERROR.YOU_HAVE_BEEN_BANNED(interaction);
+        }
+      }
+      catch (error) {
+        command.run(client, interaction);
+        console.log(chalk.white(`[COMMAND] ${interaction.guild.name}(${interaction.guild.id}) - ${interaction.user.tag}(${interaction.user.id}) executed ${interaction.commandName}`))
+      }
+    });
   }
 });

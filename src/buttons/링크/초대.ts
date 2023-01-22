ae9fd5a570d7739a7e87288ea22e1765b4ce200c
GreenScreen410 <pauljjang410@gmail.com>
2023-01-22 18:55:12 +0900

    🧹 chore: 초대 링크 수정

--- src/buttons/링크/초대.ts
+++ src/buttons/링크/초대.ts
@@ -5,5 +5,5 @@ export default {
     .setLabel("초대")
     .setEmoji("✉️")
     .setStyle(ButtonStyle.Link)
-    .setURL("https://discord.com/oauth2/authorize?client_id=767371161083314236&permissions=277028653056&scope=bot%20applications.commands")
+    .setURL("https://discord.com/api/oauth2/authorize?client_id=767371161083314236&permissions=1085824953584&scope=bot%20applications.commands")
 };

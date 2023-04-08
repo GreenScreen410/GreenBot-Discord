c7612b90e58571752c05fbf8c1db0140595ccf71
GreenScreen410 <pauljjang410@gmail.com>
2023-04-08 22:53:22 +0900

    🎨 코드 구조 개선

--- src/commands/활동/강아지.ts
+++ src/commands/활동/강아지.ts
@@ -7,19 +7,15 @@ export default {
     .setDescription("랜덤 강아지 사진을 불러옵니다."),
 
   run: async (client: Client, interaction: ChatInputCommandInteraction) => {
-    let theDogAPIData: any = await axios({
-      method: "GET",
-      url: "https://api.thedogapi.com/v1/images/search",
+    const response = await axios.get("https://api.thedogapi.com/v1/images/search", {
       headers: {
         "x-api-key": process.env.THE_DOG_API_KEY,
       },
     });
 
-    theDogAPIData = JSON.parse(JSON.stringify(theDogAPIData.data));
-
     const embed = new EmbedBuilder()
       .setColor("Random")
-      .setImage(`${theDogAPIData[0].url}`)
+      .setImage(response.data[0].url)
       .setTitle("🐶")
       .setTimestamp()
       .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });
--- src/commands/활동/고양이.ts
+++ src/commands/활동/고양이.ts
@@ -7,19 +7,15 @@ export default {
     .setDescription("랜덤 고양이 사진을 불러옵니다."),
 
   run: async (client: Client, interaction: ChatInputCommandInteraction) => {
-    let theCatAPIData: any = await axios({
-      method: "GET",
-      url: "https://api.thecatapi.com/v1/images/search",
+    const response = await axios.get("https://api.thecatapi.com/v1/images/search", {
       headers: {
         "x-api-key": process.env.THE_CAT_API_KEY,
       },
     });
 
-    theCatAPIData = JSON.parse(JSON.stringify(theCatAPIData.data));
-
     const embed = new EmbedBuilder()
       .setColor("Random")
-      .setImage(`${theCatAPIData[0].url}`)
+      .setImage(response.data[0].url)
       .setTitle("🐱")
       .setTimestamp()
       .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });

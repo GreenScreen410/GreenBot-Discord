421db4a72ddefe5d6378eb429ad4a53dfb304d15
GreenScreen410 <pauljjang410@gmail.com>
2023-04-29 21:28:52 +0900

    🔥 초대 명령어 제거

--- src/buttons/링크/초대.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonBuilder, ButtonStyle } from "discord.js";
-
-export default {
-  data: new ButtonBuilder()
-    .setLabel("초대")
-    .setEmoji("✉️")
-    .setStyle(ButtonStyle.Link)
-    .setURL("https://discord.com/api/oauth2/authorize?client_id=767371161083314236&permissions=1085824953584&scope=bot%20applications.commands")
-};

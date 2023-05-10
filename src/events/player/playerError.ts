5b70e0a3f4bba62db69cc76eedc2852740c80ad8
GreenScreen410 <pauljjang410@gmail.com>
2023-05-10 14:16:20 +0900

    🥅 player 에러 메시지 처리

--- src/events/player/playerError.ts
+++ src/events/player/playerError.ts
@@ -1,10 +1,12 @@
+import { Client, ChatInputCommandInteraction } from "discord.js";
 import { GuildQueue } from "discord-player";
 import chalk from "chalk";
 
 export default {
   name: "playerError",
 
-  execute(queue: GuildQueue, error: Error) {
+  execute(queue: GuildQueue<{ client: Client, interaction: ChatInputCommandInteraction }>, error: Error) {
     console.log(chalk.red.bold(`[PLAYER_ERROR] ${error.stack}`));
+    queue.metadata.client.error.UNKNOWN_ERROR(queue.metadata.interaction, error);
   }
 }

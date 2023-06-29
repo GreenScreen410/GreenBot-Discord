81e83152c274a21a8fb868d96d717be6b526d117
GreenScreen410 <pauljjang410@gmail.com>
2023-06-29 18:09:49 +0900

    🚑 순위표: 점수판으로 이름 변경

--- src/commands/베타/점수판.ts
+++ src/commands/베타/점수판.ts
@@ -11,26 +11,27 @@ const connection = mysql.createConnection({
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("순위표")
-    .setDescription("[베타] 활동 순위표를 확인합니다.")
+    .setName("점수판")
+    .setDescription("[베타] 활동 점수판을 확인합니다.")
     .addStringOption(option => option
       .setName("종목")
       .setDescription("종목을 선택해 주세요.")
-      .addChoices({ name: "국기퀴즈", value: "flag-quiz" })
+      .addChoices({ name: "국기퀴즈", value: "flag_quiz" })
       .setRequired(true)
     ),
 
   async execute(interaction: ChatInputCommandInteraction) {
     const activity = interaction.options.getString("종목");
 
-    if (activity === "flag-quiz") {
-      connection.query(`SELECT * FROM activity ORDER BY flag_quiz DESC`, function (error, result) {
-        const embed = new EmbedBuilder()
-          .setColor("Random")
-          .setTitle("🏆 국기퀴즈 순위표")
-          .setTimestamp()
-          .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });
+    const embed = new EmbedBuilder()
+      .setColor("Random")
+      .setTitle("🏆 국기퀴즈 순위표")
+      .setDescription("이 순위는 모든 서버에 반영됩니다!")
+      .setTimestamp()
+      .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });
 
+    if (activity === "flag_quiz") {
+      connection.query(`SELECT * FROM activity ORDER BY ${activity} DESC`, function (error, result) {
         for (let i = 0; i < result.length; i++) {
           embed.addFields({ name: `${i + 1}위`, value: `<@${result[i].id}>: ${result[i].flag_quiz}점` });
         }

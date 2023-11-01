b04889adf8848bc00bba99b6999488b6432eacd5
GreenScreen410 <pauljjang410@gmail.com>
2023-11-01 14:08:11 +0900

    ✨ 봇 사용자 차단 기능 추가

--- src/events/interaction/interactionCreate.ts
+++ src/events/interaction/interactionCreate.ts
@@ -2,27 +2,25 @@ import { Events, BaseInteraction } from "discord.js";
 import mysql from "mysql";
 import chalk from "chalk";
 
-/*
 const connection = mysql.createConnection({
-  host: `${process.env.MYSQL_HOST}`,
-  user: "root",
-  password: `${process.env.MYSQL_PASSWORD}`,
-  database: "greenbot-database",
+  host: process.env.MYSQL_HOST,
+  user: "ubuntu",
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
-*/
 
-var today = new Date();
+const today = new Date();
 
-var year = today.getFullYear();
-var month = ('0' + (today.getMonth() + 1)).slice(-2);
-var day = ('0' + today.getDate()).slice(-2);
+const year = today.getFullYear();
+const month = ('0' + (today.getMonth() + 1)).slice(-2);
+const day = ('0' + today.getDate()).slice(-2);
 
-var dateString = year + '-' + month + '-' + day;
-var hours = ('0' + today.getHours()).slice(-2);
-var minutes = ('0' + today.getMinutes()).slice(-2);
-var seconds = ('0' + today.getSeconds()).slice(-2);
+const dateString = year + '-' + month + '-' + day;
+const hours = ('0' + today.getHours()).slice(-2);
+const minutes = ('0' + today.getMinutes()).slice(-2);
+const seconds = ('0' + today.getSeconds()).slice(-2);
 
-var timeString = hours + ':' + minutes + ':' + seconds;
+const timeString = hours + ':' + minutes + ':' + seconds;
 
 console.log(dateString, timeString);
 
@@ -36,53 +34,19 @@ export default {
     await interaction.deferReply();
 
     if (!command) return interaction.client.error.INVALID_INTERACTION(interaction);
-    command.execute(interaction);
     console.log(chalk.white(`${dateString} ${timeString} - [COMMAND] ${interaction.guild.name}(${interaction.guild.id}): ${interaction.user.tag}(${interaction.user.id}) executed ${interaction.commandName}`))
 
-    /*
     try {
       connection.query(`SELECT * FROM user WHERE id=${interaction.user.id}`, function (error, result) {
-        if (result[0].banned) {
-          return interaction.client.error.YOU_HAVE_BEEN_BANNED(interaction);
-        } else {
+        if (result == "") {
           command.execute(interaction);
           console.log(chalk.white(`${dateString} ${timeString} - [COMMAND] ${interaction.guild.name}(${interaction.guild.id}): ${interaction.user.tag}(${interaction.user.id}) executed ${interaction.commandName}`))
+        } else if (result[0].banned) {
+          return interaction.client.error.YOU_HAVE_BEEN_BANNED(interaction);
         }
       });
-    } catch (error: any) {
-      console.log(error);
+    } catch (error: unknown) {
       return interaction.client.error.UNKNOWN_ERROR(interaction, error);
     }
-    */
-
-    /*
-    import mysql from "mysql";
-  
-    const connection = mysql.createConnection({
-      host: `${process.env.MYSQL_HOST}`,
-      user: "root",
-      password: `${process.env.MYSQL_PASSWORD}`,
-      database: "greenbot-database",
-    });
-  
-    if (command.permission && command.permission.length > 0) {
-      if (!interaction.guild.members.me?.permissions.has(command.permission)) {
-        const permissionList = new PermissionsBitField(command.permission).toArray()
-        return ERROR.BOT_HAVE_NO_PERMISSION(interaction, permissionList);
-      }
-    }
-  
-    connection.query(`SELECT * FROM ban WHERE ID=${interaction.user.id}`, function (error, result) {
-      try {
-        if (result[0].ban == 1) {
-          return ERROR.YOU_HAVE_BEEN_BANNED(interaction);
-        }
-      }
-      catch (error) {
-        command.run(client, interaction);
-        console.log(chalk.white(`[COMMAND] ${interaction.guild.name}(${interaction.guild.id}) - ${interaction.user.tag}(${interaction.user.id}) executed ${interaction.commandName}`))
-      }
-    });
-    */
   }
 }

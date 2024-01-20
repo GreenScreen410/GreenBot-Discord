03dfb4c349d3c8d9456743b1c83a5d652ca6fc39
GreenScreen410 <pauljjang410@gmail.com>
2024-01-20 16:48:55 +0900

    🐛 명령어 실행 시간 로그가 업데이트되지 않던 현상 수정

--- src/events/interaction/interactionCreate.ts
+++ src/events/interaction/interactionCreate.ts
@@ -2,16 +2,6 @@ import { Events, type BaseInteraction } from 'discord.js'
 import mysql from 'mysql2/promise'
 import chalk from 'chalk'
 
-const today = new Date()
-const year = today.getFullYear()
-const month = ('0' + (today.getMonth() + 1)).slice(-2)
-const day = ('0' + today.getDate()).slice(-2)
-const dateString = year + '-' + month + '-' + day
-const hours = ('0' + today.getHours()).slice(-2)
-const minutes = ('0' + today.getMinutes()).slice(-2)
-const seconds = ('0' + today.getSeconds()).slice(-2)
-const timeString = hours + ':' + minutes + ':' + seconds
-
 export default {
   name: Events.InteractionCreate,
 
@@ -36,6 +26,17 @@ export default {
     const [result]: any = await connection.query(`SELECT * FROM user WHERE id=${interaction.user.id}`)
     if (result[0] == null || result[0].banned === 0) {
       await command.execute(interaction)
+
+      const today = new Date()
+      const year = today.getFullYear()
+      const month = ('0' + (today.getMonth() + 1)).slice(-2)
+      const day = ('0' + today.getDate()).slice(-2)
+      const dateString = year + '-' + month + '-' + day
+      const hours = ('0' + today.getHours()).slice(-2)
+      const minutes = ('0' + today.getMinutes()).slice(-2)
+      const seconds = ('0' + today.getSeconds()).slice(-2)
+      const timeString = hours + ':' + minutes + ':' + seconds
+
       console.log(chalk.white(`${dateString} ${timeString} - [COMMAND] ${interaction.guild.name}(${interaction.guild.id}): ${interaction.user.tag}(${interaction.user.id}) executed ${interaction.commandName}`))
     } else if (result[0].banned === 1) {
       await interaction.client.error.YOU_HAVE_BEEN_BANNED(interaction, 'Test Message, 이게 나온다면 @6r33n을 멘션해 주세요.')

a0e02c17c485c2250f052fb3ce44e0db88dfdd7e
GreenScreen410 <pauljjang410@gmail.com>
2024-10-11 09:03:04 +0900

    🎨 MySQL 핸들러 재구성

--- src/events/interaction/interactionCreate.ts
+++ src/events/interaction/interactionCreate.ts
@@ -7,24 +7,26 @@ export default {
     if (!interaction.isChatInputCommand() || !interaction.inCachedGuild()) return
 
     const command = interaction.client.commands.get(interaction.commandName)
-    await interaction.deferReply()
-
     if (command == null) {
       return await interaction.client.error.INVALID_INTERACTION(interaction)
     }
 
-    const [result]: any = await interaction.client.mysql.query(`SELECT banned FROM user WHERE id=${interaction.user.id}`)
+    await interaction.deferReply()
+
     try {
-      if (result[0].banned === 1) {
-        return await interaction.client.error.YOU_HAVE_BEEN_BANNED(interaction, 'Test Message, 이게 나온다면 @6r33n을 멘션해 주세요.')
+      const banned = (await interaction.client.mysql.query(`SELECT banned FROM user WHERE id = ${interaction.user.id}`)).banned
+      if (banned === 1) {
+        const reason: string = (await interaction.client.mysql.query(`SELECT banned_reason FROM user WHERE id = ${interaction.user.id}`)).banned_reason
+        return await interaction.client.error.YOU_HAVE_BEEN_BANNED(interaction, reason)
       }
     } catch (error) {
-      await interaction.client.mysql.query(`INSERT INTO user VALUES (${interaction.user.id}, 0)`)
+      await interaction.client.mysql.register(interaction)
     }
 
     await command.execute(interaction)
 
     console.log(`[InteractionCreate] ${interaction.guild.name}(${interaction.guild.id}): ${interaction.user.tag}(${interaction.user.id}) executed ${interaction.commandName}`)
-    await interaction.client.mysql.query('UPDATE statistics SET count=count+1 WHERE event="total_command"')
+    await interaction.client.mysql.query('UPDATE statistics SET count = count + 1 WHERE event = "total_command"')
+    await interaction.client.mysql.query(`UPDATE user SET count = count + 1 WHERE id = "${interaction.user.id}"`)
   }
 }

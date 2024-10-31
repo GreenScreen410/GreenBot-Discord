0b8cb0e5e9f7f2916758b3faba04c447cbffe4c9
GreenScreen410 <pauljjang410@gmail.com>
2024-11-01 08:17:27 +0900

    ✨ discord-player에서 Lavalink로 변경

--- src/commands/음악/loop.ts
+++ src/commands/음악/loop.ts
@@ -1,13 +1,24 @@
 import { type ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder } from 'discord.js'
-import { useQueue, QueueRepeatMode } from 'discord-player'
 
 export default {
   data: new SlashCommandBuilder()
-    .setName('반복')
-    .setDescription('재생중인 노래를 반복합니다.')
+    .setName('loop')
+    .setNameLocalizations({
+      ko: '반복'
+    })
+    .setDescription('Loop the current track or the whole queue.')
+    .setDescriptionLocalizations({
+      ko: '재생중인 음악을 반복합니다.'
+    })
     .addStringOption(option => option
-      .setName('유형')
-      .setDescription('필터를 지정해 주세요.')
+      .setName('type')
+      .setNameLocalizations({
+        ko: '유형'
+      })
+      .setDescription('Specify the target to repeat.')
+      .setDescriptionLocalizations({
+        ko: '반복할 대상을 지정해 주세요.'
+      })
       .addChoices({ name: '끄기', value: 'off' })
       .addChoices({ name: '트랙', value: 'track' })
       .addChoices({ name: '재생목록', value: 'queue' })
@@ -15,40 +26,40 @@ export default {
     ),
 
   async execute (interaction: ChatInputCommandInteraction<'cached'>) {
-    const queue = useQueue(interaction.guildId)
-    if (queue?.currentTrack == null) {
+    const player = interaction.client.lavalink.players.get(interaction.guildId)
+    if (player == null) {
       return await interaction.client.error.MUSIC_QUEUE_IS_EMPTY(interaction)
     }
     if (interaction.guild.members.me?.voice.channelId != null && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) {
       return await interaction.client.error.PLEASE_JOIN_SAME_VOICE_CHANNEL(interaction)
     }
 
-    const option = interaction.options.getString('유형')
+    const option = interaction.options.getString('type')
 
     if (option === 'track') {
-      queue.setRepeatMode(QueueRepeatMode.TRACK)
+      await player.setRepeatMode('track')
       const embed = new EmbedBuilder()
         .setColor('Random')
         .setTitle('🔁 현재 재생중인 음악을 반복 재생합니다!')
-        .setDescription(queue.currentTrack.title)
+        .setDescription(player.queue.tracks[0].info.title)
       await interaction.followUp({ embeds: [embed] })
     }
 
     if (option === 'queue') {
-      queue.setRepeatMode(QueueRepeatMode.QUEUE)
+      await player.setRepeatMode('queue')
       const embed = new EmbedBuilder()
         .setColor('Random')
         .setTitle('🔁 전체 대기열을 반복 재생합니다!')
-        .setDescription(`${queue.currentTrack.title} 외 ${queue.tracks.toArray().length}개의 음악`)
+        .setDescription(`${player.queue.tracks[0].info.title} 외 ${player.queue.tracks.length}개의 음악`)
       await interaction.followUp({ embeds: [embed] })
     }
 
     if (option === 'off') {
-      queue.setRepeatMode(QueueRepeatMode.OFF)
+      await player.setRepeatMode('off')
       const embed = new EmbedBuilder()
         .setColor('Random')
         .setTitle('🔁 반복모드가 꺼졌습니다!')
-        .setDescription(`${queue.currentTrack?.title}`)
+        .setDescription(player.queue.tracks[0].info.title)
       await interaction.followUp({ embeds: [embed] })
     }
   }

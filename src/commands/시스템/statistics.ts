9895717bc74a807e3f06c116be51cf90a7ad133c
GreenScreen410 <pauljjang410@gmail.com>
2024-10-31 15:18:39 +0900

    🎨 통계: 영어 번역 추가, 3개 항목 추가

--- src/commands/시스템/statistics.ts
+++ src/commands/시스템/statistics.ts
@@ -12,6 +12,7 @@ export default {
     }),
 
   async execute (interaction: ChatInputCommandInteraction) {
+    const date = new Date()
     let totalSeconds = (interaction.client.uptime / 1000)
     const days = Math.floor(totalSeconds / 86400)
     totalSeconds %= 86400
@@ -20,9 +21,10 @@ export default {
     const minutes = Math.floor(totalSeconds / 60)
     const seconds = Math.floor(totalSeconds % 60)
 
-    const date = new Date()
-    const totalCommand = await interaction.client.mysql.query('SELECT count FROM statistics WHERE event="total_command"')
+    const totalCommand = await interaction.client.mysql.query('SELECT count FROM statistics WHERE event = "total_command"')
     const uniqueUser = await interaction.client.mysql.query('SELECT COUNT(*) FROM user')
+    const bannedUser = await interaction.client.mysql.query('SELECT COUNT(*) FROM user WHERE banned = 1')
+    const mostCommandUser = await interaction.client.mysql.query('SELECT id FROM user ORDER BY count DESC LIMIT 1')
 
     const embed = new EmbedBuilder()
       .setColor('Random')
@@ -35,7 +37,10 @@ export default {
         { name: '📜 전체 채널 수', value: `${interaction.client.channels.cache.size}개`, inline: true },
         { name: '🔧 전체 명령어 수', value: `${interaction.client.commands.size}개`, inline: true },
         { name: '🔧 총 명령어 실행 횟수', value: `${totalCommand.count}회`, inline: true },
-        { name: '🕒 가동 시간', value: `${days}일 ${hours}시간  ${minutes}분 ${seconds}초`, inline: true }
+        { name: '🕒 가동 시간', value: `${days}일 ${hours}시간  ${minutes}분 ${seconds}초`, inline: true },
+        { name: '🚫 차단된 유저 수', value: `${bannedUser['COUNT(*)']}명`, inline: true },
+        { name: '🎵 현재 재생중인 음악 서버 수', value: `${interaction.client.lavalink.players.size}개`, inline: true },
+        { name: '🏆 가장 많이 명령어를 사용한 사용자', value: `<@${mostCommandUser.id}>` }
       )
     await interaction.followUp({ embeds: [embed] })
   }

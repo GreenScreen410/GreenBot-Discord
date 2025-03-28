6ae3e3ef233c5df2ed37284916d7406ee67acfc0
GreenScreen410 <pauljjang410@gmail.com>
2025-03-28 09:56:53 +0900

    🎨 Lavalink, Logger 파일 분리 및 Sharding

---
+++ src/handler/lavalink.ts
@@ -0,0 +1,38 @@
+import { LavalinkManager } from 'lavalink-client'
+import type { Client } from 'discord.js'
+
+export default function (client: Client): LavalinkManager {
+  return new LavalinkManager({
+    nodes: [
+      {
+        id: 'Local Node',
+        host: `${process.env.SERVER_IP}`,
+        port: 2333,
+        authorization: `${process.env.LAVALINK_PASSWORD}`,
+        retryAmount: 5,
+        retryDelay: 60000,
+        secure: false
+      }
+    ],
+    sendToShard: (guildId, payload) => client.guilds.cache.get(guildId)?.shard?.send(payload),
+    // everything down below is optional
+    autoSkip: true,
+    playerOptions: {
+      clientBasedPositionUpdateInterval: 150,
+      defaultSearchPlatform: 'ytsearch',
+      volumeDecrementer: 0.75,
+      // requesterTransformer: requesterTransformer,
+      onDisconnect: {
+        autoReconnect: true,
+        destroyPlayer: false
+      },
+      onEmptyQueue: {
+        destroyAfterMs: 0
+        // autoPlayFunction: autoPlayFunction,
+      }
+    },
+    queueOptions: {
+      maxPreviousTracks: 25
+    }
+  })
+}

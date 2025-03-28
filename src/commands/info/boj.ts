0d2078689bd163af76c29c744024e2379c7f399d
GreenScreen410 <pauljjang410@gmail.com>
2025-03-28 10:21:12 +0900

    ✨ 백준: 유저 정보 기능 추가, 문제 태그 스포일러 방지 추가

--- src/commands/info/boj.ts
+++ src/commands/info/boj.ts
@@ -1,6 +1,7 @@
-import { type ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder, type ColorResolvable } from 'discord.js'
+import { type ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder, time, type ColorResolvable } from 'discord.js'
 import axios from 'axios'
 import { load } from 'cheerio'
+import dayjs from 'dayjs'
 
 interface ProblemResponse {
   problemId: number
@@ -9,6 +10,27 @@ interface ProblemResponse {
   acceptedUserCount: number
   averageTries: number
   tags: Array<{ displayNames: Array<{ name: string }> }>
+}
+
+interface UserResponse {
+  handle: string
+  profileImageUrl: string
+  tier: number
+  rating: number
+  class: number
+  rank: number
+  solvedCount: number
+  voteCount: number
+  rivalCount: number
+  reverseRivalCount: number
+  stardusts: number
+  coins: number
+  proUntil: string
+  backgroundId: number
+}
+
+interface BackgroundResponse {
+  backgroundImageUrl: string
 }
 
 const tierData = {
@@ -20,7 +42,6 @@ const tierData = {
   diamond: { color: '#2eb2f6', emoji: ['<:diamond5:1236286916257644574>', '<:diamond4:1236286914081067039>', '<:diamond3:1236286912956862534>', '<:diamond2:1236286911459491871>', '<:diamond1:1236286909836427408>'] },
   ruby: { color: '#f72664', emoji: ['<:ruby5:1236286950626037791>', '<:ruby4:1236286947924774933>', '<:ruby3:1236286944913264731>', '<:ruby2:1236287150069125192>', '<:ruby1:1236286940102398002>'] }
 }
-
 const tier = [tierData.unranked.emoji, ...Object.values(tierData).slice(1).flatMap(t => t.emoji)]
 const tierColor = [tierData.unranked.color, ...Object.values(tierData).slice(1).flatMap(t => Array(5).fill(t.color))]
 
@@ -32,48 +53,106 @@ export default {
     })
     .setDescription('Loads information about a Baekjoon Online Judge problem.')
     .setDescriptionLocalizations({
-      ko: '백준 문제 정보를 불러옵니다.'
+      ko: '백준/solved.ac 정보를 불러옵니다.'
     })
-    .addIntegerOption((option) => option
-      .setName('id')
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('problem')
+        .setNameLocalizations({
+          ko: '문제'
+        })
+        .setDescription('Loads information about a Baekjoon Online Judge problem.')
+        .setDescriptionLocalizations({
+          ko: '백준 문제 정보를 불러옵니다.'
+        })
+        .addIntegerOption((option) => option
+          .setName('id')
+          .setNameLocalizations({
+            ko: '문제'
+          })
+          .setDescription('Enter the problem ID. If you do not enter a problem ID, a problem will be loaded randomly.')
+          .setDescriptionLocalizations({
+            ko: '문제 ID를 입력해 주세요. 문제 ID를 입력하지 않을 시, 랜덤으로 문제를 불러옵니다.'
+          })
+          .setMinValue(1000)
+        )
+    )
+    .addSubcommand(subcommand => subcommand
+      .setName('user')
       .setNameLocalizations({
-        ko: '문제'
+        ko: '유저'
       })
-      .setDescription('Enter the problem ID.')
+      .setDescription('Loads information about a Baekjoon Online Judge user.')
       .setDescriptionLocalizations({
-        ko: '문제 ID를 입력해 주세요.'
+        ko: '백준 유저 정보를 불러옵니다.'
       })
-      .setMinValue(1000)
-      .setRequired(true)
+      .addStringOption((option) => option
+        .setName('handle')
+        .setNameLocalizations({
+          ko: '핸들'
+        })
+        .setDescription('Enter the user handle.')
+        .setDescriptionLocalizations({
+          ko: '사용자 핸들을 입력해 주세요.'
+        })
+        .setRequired(true)
+      )
     ),
 
   async execute (interaction: ChatInputCommandInteraction) {
-    const problemID = interaction.options.getInteger('id', true)
+    if (interaction.options.getSubcommand() === 'problem') {
+      const maxProblemResponse = await axios.get('https://www.acmicpc.net/problem/added', { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0' } })
+      const $ = load(maxProblemResponse.data as string)
+      const maxProblemID = Number($('#problemset > tbody > tr:nth-child(1) > td.list_problem_id').text())
+      const problemID = interaction.options.getInteger('id') ?? Math.floor(Math.random() * maxProblemID) + 1000
+      if (problemID > maxProblemID) {
+        return await interaction.client.error.INVALID_ARGUMENT(interaction, `${maxProblemID}번 문제까지만 조회하실 수 있습니다.`)
+      }
 
-    const maxProblemResponse = await axios.get('https://www.acmicpc.net/problem/added', { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0' } })
-    const $ = load(maxProblemResponse.data as string)
-    const maxProblemID = Number($('#problemset > tbody > tr:nth-child(1) > td.list_problem_id').text())
-    if (problemID > maxProblemID) {
-      return await interaction.client.error.INVALID_ARGUMENT(interaction, `${maxProblemID}번 문제까지만 조회하실 수 있습니다.`)
+      const { data: response } = await axios.get<ProblemResponse>(`https://solved.ac/api/v3/problem/show?problemId=${problemID}`)
+
+      const embed = new EmbedBuilder()
+        .setURL(`https://www.acmicpc.net/problem/${problemID}`)
+        .setColor(tierColor[response.level] as ColorResolvable)
+        .setTitle(`${response.problemId} - ${response.titleKo}`)
+        .setDescription(`난이도: ${tier[response.level]}`)
+        .addFields(
+          { name: '<:ac:1236283747045998675> 맞은 사람', value: response.acceptedUserCount.toLocaleString(), inline: true },
+          { name: '🔁 평균 시도 횟수', value: response.averageTries.toLocaleString(), inline: true }
+        )
+
+      if (response.tags.length > 0) {
+        const tags = response.tags.map((tag: { displayNames: Array<{ name: string }> }) => tag.displayNames[0].name).join('\n')
+        embed.addFields({ name: '📛 알고리즘 분류', value: `||${tags}||` })
+      }
+
+      await interaction.followUp({ embeds: [embed] })
+    } else if (interaction.options.getSubcommand() === 'user') {
+      const handle = interaction.options.getString('handle')
+
+      const { data: user } = await axios.get<UserResponse>(`https://solved.ac/api/v3/user/show?handle=${handle}`)
+      const { data: background } = await axios.get<BackgroundResponse>(`https://solved.ac/api/v3/background/show?backgroundId=${user.backgroundId}`)
+
+      const embed = new EmbedBuilder()
+        .setURL(`https://solved.ac/profile/${handle}`)
+        .setThumbnail(user.profileImageUrl ?? 'https://static.solved.ac/misc/360x360/default_profile.png')
+        .setColor(tierColor[user.tier] as ColorResolvable)
+        .setTitle(`${tier[user.tier]} ${user.handle}`)
+        .setDescription(user.rating.toLocaleString())
+        .addFields(
+          { name: '🔢 클래스', value: user.class.toLocaleString(), inline: true },
+          { name: '🏆 순위', value: user.rank.toLocaleString(), inline: true },
+          { name: '✏️ 해결한 문제', value: user.solvedCount.toLocaleString(), inline: true },
+          { name: '🤝 기여한 문제', value: user.voteCount.toLocaleString(), inline: true },
+          { name: '👤 라이벌', value: user.rivalCount.toLocaleString(), inline: true },
+          { name: '👤 역라이벌', value: user.reverseRivalCount.toLocaleString(), inline: true },
+          { name: '🌟 별조각', value: user.stardusts.toLocaleString(), inline: true },
+          { name: '🪙 코인', value: (user.coins * 0.01).toLocaleString(), inline: true },
+          { name: '♥️ 프로 기간', value: time(dayjs(user.proUntil).unix()), inline: true }
+        )
+        .setImage(background.backgroundImageUrl)
+
+      await interaction.followUp({ embeds: [embed] })
     }
-
-    const { data: response } = await axios.get<ProblemResponse>(`https://solved.ac/api/v3/problem/show?problemId=${problemID}`)
-
-    const embed = new EmbedBuilder()
-      .setURL(`https://www.acmicpc.net/problem/${problemID}`)
-      .setColor(tierColor[response.level] as ColorResolvable)
-      .setTitle(`${response.problemId} - ${response.titleKo}`)
-      .setDescription(`난이도: ${tier[response.level]}`)
-      .addFields(
-        { name: '<:ac:1236283747045998675> 맞은 사람', value: `${response.acceptedUserCount}`, inline: true },
-        { name: '🔁 평균 시도 횟수', value: `${response.averageTries}`, inline: true }
-      )
-
-    if (response.tags.length > 0) {
-      const tags = response.tags.map((tag: { displayNames: Array<{ name: string }> }) => tag.displayNames[0].name).join('\n')
-      embed.addFields({ name: '📛 알고리즘 분류', value: tags })
-    }
-
-    await interaction.followUp({ embeds: [embed] })
   }
 }

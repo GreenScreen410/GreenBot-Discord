93a8d7b251dbbb18a1f2507780a42eb3ee03164a
GreenScreen410 <pauljjang410@gmail.com>
2025-06-02 17:37:44 +0900

    ✨ 재생중: 재생 진행도 추가

--- src/commands/music/nowplaying.ts
+++ src/commands/music/nowplaying.ts
@@ -1,8 +1,17 @@
 import { type ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder } from 'discord.js'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration.js'
+dayjs.extend(duration)
 
-function msToTime (s: number): string {
-  const pad = (n: any, z = 2): string => ('00' + n).slice(-z)
-  return pad(s / 3.6e6 | 0) + ':' + pad((s % 3.6e6) / 6e4 | 0) + ':' + pad((s % 6e4) / 1000 | 0)
+function progressBar (current: number, total: number, size = 14): string {
+  if (current > total) current = total
+  const currentPosition = Math.round((size * current) / total)
+  const bar = '▬'.repeat(currentPosition) + '🔘' + '▬'.repeat(size - currentPosition)
+  const currentTimeFormattedRaw = dayjs.duration(current).format('HH:mm:ss')
+  const totalTimeFormattedRaw = dayjs.duration(total).format('HH:mm:ss')
+  const currentTimeFormatted = currentTimeFormattedRaw.startsWith('00:') ? currentTimeFormattedRaw.substring(3) : currentTimeFormattedRaw
+  const totalTimeFormatted = totalTimeFormattedRaw.startsWith('00:') ? totalTimeFormattedRaw.substring(3) : totalTimeFormattedRaw
+  return `${currentTimeFormatted} ┃ ${bar} ┃ ${totalTimeFormatted}`
 }
 
 export default {
@@ -19,22 +28,24 @@ export default {
   async execute (interaction: ChatInputCommandInteraction<'cached'>) {
     const player = interaction.client.lavalink.getPlayer(interaction.guildId)
     if (player?.queue.current == null) {
-      return await interaction.client.error.MUSIC_QUEUE_IS_EMPTY(interaction)
+      return interaction.client.error.MUSIC_QUEUE_IS_EMPTY(interaction)
     }
     if (interaction.guild.members.me?.voice.channelId != null && interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId) {
-      return await interaction.client.error.PLEASE_JOIN_SAME_VOICE_CHANNEL(interaction)
+      return interaction.client.error.PLEASE_JOIN_SAME_VOICE_CHANNEL(interaction)
     }
 
+    const bar = progressBar(player.position, player.queue.current.info.duration)
+    const percent = Math.round((player.position / player.queue.current.info.duration) * 100)
     const embed = new EmbedBuilder()
       .setURL(player.queue.current.info.uri)
       .setColor('Random')
-      .setTitle(await interaction.client.locale(interaction, 'command.nowplaying.title'))
+      .setTitle(await interaction.client.i18n(interaction, 'command.nowplaying.title'))
       .setDescription(player.queue.current.info.title)
       .setThumbnail(player.queue.current.info.artworkUrl ?? '')
       .addFields([
-        { name: await interaction.client.locale(interaction, 'command.nowplaying.author'), value: player.queue.current.info.author },
-        { name: await interaction.client.locale(interaction, 'command.nowplaying.duration'), value: msToTime(player.queue.current.info.duration) },
-        { name: await interaction.client.locale(interaction, 'command.nowplaying.source_name'), value: player.queue.current.info.sourceName }
+        { name: await interaction.client.i18n(interaction, 'command.nowplaying.author'), value: player.queue.current.info.author },
+        { name: await interaction.client.i18n(interaction, 'command.nowplaying.progress', { percent }), value: bar },
+        { name: await interaction.client.i18n(interaction, 'command.nowplaying.source_name'), value: player.queue.current.info.sourceName }
       ])
     await interaction.followUp({ embeds: [embed] })
   }

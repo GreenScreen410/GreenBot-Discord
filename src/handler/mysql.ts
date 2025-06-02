6fe4dc021b162a594318a3e984bcfc832b7e2711
GreenScreen410 <pauljjang410@gmail.com>
2025-06-02 17:35:22 +0900

    🗃️ Prisma 시범 적용 (MySQL은 점진적으로 퇴출 예정)

--- src/handler/mysql.ts
+++ src/handler/mysql.ts
@@ -1,4 +1,5 @@
 import mysql from 'mysql2/promise'
+import logger from '../handler/logger.js'
 
 const connection = mysql.createPool({
   host: process.env.SERVER_IP,
@@ -11,16 +12,12 @@ const connection = mysql.createPool({
 export default {
   register: async function (interaction: any) {
     await connection.query(`INSERT INTO user VALUES (${interaction.user.id}, 0, '', 0, '')`)
-    interaction.client.logger.info(`[MySQL] ${interaction.user.tag}(${interaction.user.id}) has been registered.`)
+    logger.info(`[MySQL] ${interaction.user.tag}(${interaction.user.id}) has been registered.`)
   },
 
   query: async function (query: string, values: any) {
     const [rows]: any = await connection.query(query, values)
-
-    if (rows.length === 1) {
-      return rows[0]
-    } else {
-      return rows
-    }
+    if (rows.length === 1) return rows[0]
+    else return rows
   }
 }

1a59c2a54efe27ee42c6a83a0b85bdddea9eaed6
GreenScreen410 <pauljjang410@gmail.com>
2025-06-03 02:37:01 +0900

    🎨 logger 재설계

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -5,5 +5,10 @@ declare namespace NodeJS {
     THE_CAT_API_KEY: string
     THE_DOG_API_KEY: string
     ADMIN_ID: string
+    CLIENT_ID: string
+    GUILD_ID: string
+    SERVER_IP: string
+    LAVALINK_PASSWORD: string
+    LAVALINK_PORT: number
   }
 }
